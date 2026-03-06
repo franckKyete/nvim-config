56fe7964f40597748f2139cb713bb4b1f1b7e2da
franckKyete <franck.kyete@gmail.com>
2026-03-06 23:19:01 +0200

    Remape floating terminal keybindings to <M-*>

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -18,19 +18,19 @@ return {
 
             local overlay = require("configs.terminal_overlay")
 
-            vim.keymap.set("n", "<leader>to", overlay.toggle, { desc = "Toggle terminal overlay" })
-            vim.keymap.set({"n", "t"}, "<leader>ta", overlay.add, { desc = "Add terminal to overlay" })
-            vim.keymap.set("n", "<leader>tr", function() overlay.remove() end,
+            vim.keymap.set({"n", "t"}, "<M-t>", overlay.toggle, { desc = "Toggle terminal overlay" })
+            vim.keymap.set({"n", "t"}, "<M-a>", overlay.add, { desc = "Add terminal to overlay" })
+            vim.keymap.set("n", "<M-r>", function() overlay.remove() end,
                 { desc = "Remove last terminal from overlay" })
-            vim.keymap.set("n", "<leader>tz", overlay.toggle_zoom, { desc = "Zoom terminal in overlay" })
+            vim.keymap.set("n", "<M-z>", overlay.toggle_zoom, { desc = "Zoom terminal in overlay" })
 
-            vim.keymap.set("n", "<leader>t1", function() overlay.focus(1) end, { desc = "Focus terminal 1" })
-            vim.keymap.set("n", "<leader>t2", function() overlay.focus(2) end, { desc = "Focus terminal 2" })
-            vim.keymap.set("n", "<leader>t3", function() overlay.focus(3) end, { desc = "Focus terminal 3" })
-            vim.keymap.set("n", "<leader>t4", function() overlay.focus(4) end, { desc = "Focus terminal 4" })
+            vim.keymap.set({"n", "t"}, "<M-1>", function() overlay.focus(1) end, { desc = "Focus terminal 1" })
+            vim.keymap.set({"n", "t"}, "<M-2>", function() overlay.focus(2) end, { desc = "Focus terminal 2" })
+            vim.keymap.set({"n", "t"}, "<M-3>", function() overlay.focus(3) end, { desc = "Focus terminal 3" })
+            vim.keymap.set({"n", "t"}, "<M-4>", function() overlay.focus(4) end, { desc = "Focus terminal 4" })
 
             vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
-            vim.keymap.set("t", "<leader>tz", function()
+            vim.keymap.set("t", "<M-z>", function()
                 vim.cmd("stopinsert")
                 overlay.toggle_zoom()
             end, { desc = "Zoom terminal in overlay" })
